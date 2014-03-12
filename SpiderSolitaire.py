50d64ecdd209abf0c04c868b026fdd4bcb20faaf
BjornBjarnsteins <bjb46@hi.is>
2014-03-12 12:28:48 +0000

    bætti við smá skýringum

--- SpiderSolitaire.py
+++ SpiderSolitaire.py
@@ -5,12 +5,14 @@
 #to move one or more cards from one stack to the other (uses SpiderStack's add() and remove() )
 #def move()
 
-#def calcScore()
 
 #Uses SpiderDeck's remove() and SpiderStack's add() to take 10 cards from the hidden pile and put one revealed on each of the ten stacks
 #def deal() 
 
+#vika 2
+#def calcScore()
 
+# Vika 2:
 #for storing and loading high scores into txt file:
 #def store()
 #def load()
