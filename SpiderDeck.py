bf8cc393711df6a8db69a69bdfa284318e5b1f48
BjornBjarnsteins <bjb46@hi.is>
2014-03-13 15:48:06 +0000

    SpiderDeck klasinn kominn

--- SpiderDeck.py
+++ SpiderDeck.py
@@ -5,13 +5,51 @@
 #In the case of all four suits, each sequence will appear 2 times.
 
 #All in all: It will always take 8 complete sequences to win the game, no matter what suit or difficulty
+from SpiderCard import Card
+import random
 
-
-#Methods:
-
-#Shuffles the deck:
-#def shuffle():
-
-#Removes the cards that are put in play from the deck.
-#def remove():
-
+class SpiderDeck:
+    
+    # Use:  deck = SpiderDeck(n)
+    # Pre:  n is a number, 1, 2 or 4
+    def __init__(self, suitNo, decklength=104):
+        # decklist er listi af Card hlutum
+        self.decklist = []
+        self.suits = []
+        if suitNo == 1:
+            suits = ['S']
+        elif suitNo == 2:
+            suits = ['S', 'H']
+        elif suitNo == 4:
+            suits = ['S', 'H', 'D', 'C']
+            
+        for n in range(0, 8/suitNo):
+            for suit in suits:
+                for rank in range(0, 13):
+                    self.decklist.append(Card(suit, rank))
+            
+    # Use:  deck.shuffle()
+    # Post: deck is shuffled        
+    def shuffle(self):
+        random.shuffle(self.decklist)
+        
+    # Use:  cards = deck.remove()
+    # Post: the card on the top of the deck has been removed
+    def remove(self):
+        return self.decklist.pop()
+                    
+if __name__=="__main__":
+    #Testing functions
+    deck = SpiderDeck(4)
+    deck.shuffle()
+    for c in deck.decklist:
+        print c
+    
+    print
+    print
+    
+    print len(deck.decklist)
+    
+    print deck.remove()
+    for n in range(0, 8):
+        print deck.remove()
